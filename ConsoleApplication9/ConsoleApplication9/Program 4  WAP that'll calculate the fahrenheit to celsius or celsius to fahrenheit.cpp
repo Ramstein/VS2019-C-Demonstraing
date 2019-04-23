5d36c7e88efe70aa3ad56d9e2b9d317c0f327b7d
Ramstein <46052567+RamsteinWR@users.noreply.github.com>
2019-04-24 04:36:29 +0530

    Add files via upload

---
+++ ConsoleApplication9/ConsoleApplication9/Program 4  WAP that'll calculate the fahrenheit to celsius or celsius to fahrenheit.cpp
@@ -0,0 +1,32 @@
+/*PROBLEM SOLVING THROUGH PROGRAMMING IN C
+Program =4  WAP that'll calculate the fahrenheit to celsius or celsius to fahrenheit:
+Name= Md Zeeshan
+Section= A5/16
+submitted to- Ms. Bhargavi singh*/
+
+#include"pch.h"
+#include <iostream>
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+	double c, f;
+	char ch;
+	printf("\nFor Conversion from celsius to fahrenhite put f\nOr for fahrenhite to celsius put c: ");
+	scanf_s("%c", &ch);
+	if (ch == 'f') {
+		printf("\n Enter celsius value: ");
+		scanf_s("%lf", &c);
+		f = (1.8*c) + 32;
+		printf("Fahrenhite=%f", f);
+	}
+	else if (ch == 'c'){
+		printf("\n Enter Fahrenhite value: ");
+		scanf_s("%lf", &f);
+		c = (f - 32) / 1.8;
+		printf("Celsius=%f", c);
+	}
+	return 0;
+}
