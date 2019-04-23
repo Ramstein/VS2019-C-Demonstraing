390796affbc0180c44e8de0bbfc03741efb26c27
Ramstein <46052567+RamsteinWR@users.noreply.github.com>
2019-04-24 04:31:04 +0530

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/Calculating the simple Interest.c
@@ -0,0 +1,39 @@
+
+
+#include "pch.h"
+#include <iostream>
+#include<conio.h>
+#include<math.h>
+
+
+int main()
+{
+		  
+
+	for (int i = 0; i < 100; i++)
+		printf("|nHellow world!");
+
+	//Calculating the simple interest
+	int p, n, r,A;
+	float ci;
+	float si;
+	printf("\n Enter the principal,time, and rate of interest: ");
+	scanf_s("%d%d%d", &p, &n, &r);
+	si = p * n*r;
+	printf("Simple interest=%f", si);
+	//calculating the compund interest
+	
+	A = p * pow((1 + (r / 100)), n);
+	ci = A-p;
+
+	printf("\nCompound interest=%f", ci);
+
+	//finding the roots of quadratic equations
+	
+
+
+
+
+	return 0;
+	
+}
