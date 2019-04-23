d7cec6ec833be4ae8a1208aaf217b1edfe5496ea
Ramstein <46052567+RamsteinWR@users.noreply.github.com>
2019-04-24 04:56:16 +0530

    Add files via upload

---
+++ ConsoleApplication29/ConsoleApplication29/Program =24 WAP to search an element in an array using linear search.cpp
@@ -0,0 +1,33 @@
+/*PROBLEM SOLVING THROUGH PROGRAMMING IN C
+Program =24 WAP to search an element in an array using linear search
+Name= Md Zeeshan
+Section= A5/16
+submitted to- Ms. Bhargavi singh*/
+
+#include"pch.h"
+#include <iostream>
+#include<stdio.h>
+#include<conio.h>
+int main() {
+	int num[10] = {};
+	int find, k=0;
+	printf("\n Make an array and search an element in that.\n");
+	for (int i = 0; i < 10; i++) {
+		printf(" Put Num%d: ", i + 1);
+		scanf_s("%d", &num[i]);
+	}
+	printf(" What element do you want to search: ");
+	scanf_s("%d", &find);
+	for (int i = 0; i < 10; i++) {
+		if (num[i] == find) {
+			printf("\n %d is +nt at %d location in the array.", find, i + 1);
+			k++;
+			break;
+		}
+	}
+	if (k !=1) {
+		printf("\n %d is not +nt in the array.", find);
+	}
+	return 0;
+}
+
